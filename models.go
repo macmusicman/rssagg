5a9e81a76b4fb9a1dbf8ab761c6c55507691eefa
Martinmusicman <16878013+macmusicman@users.noreply.github.com>
2024-01-08 21:59:39 +0000

    added new file

--- models.go
+++ models.go
@@ -12,7 +12,7 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
-	APIKey    string    `json: "api_key"`
+	APIKey    string    `json:"api_key"`
 }
 
 func databaseUserToUser(dbUser database.User) User {
